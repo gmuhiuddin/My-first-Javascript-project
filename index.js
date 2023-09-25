7c2c24d49203f99e6201a2a86aff3f9b6b2b9692
Ghulam_muhiuddin <muhiuddinghulam825@gmail.com>
2023-09-25 19:02:37 +0500

    added work

---
+++ index.js
@@ -0,0 +1,27 @@
+var amount = 0;
+var amount1 = 0;
+
+function expenceCalulatorIncome (){
+    let enterAmount = document.getElementById("enterAmount").value
+    let incomeZero = document.getElementById("incomeZero")
+    let expenceZero = document.getElementById("expenceZero")
+    let profitLossZero = document.getElementById("profitLossZero")
+    enterAmount = +enterAmount
+
+    amount += enterAmount
+    incomeZero.innerText = amount
+    profitLossZero.innerText = amount
+
+}
+function expenceCalulatorExpence (){
+    
+
+    let enterAmount = document.getElementById("enterAmount").value
+    let incomeZero = document.getElementById("incomeZero")
+    let expenceZero = document.getElementById("expenceZero")
+    let profitLossZero = document.getElementById("profitLossZero")
+    enterAmount = +enterAmount
+        amount1 += enterAmount
+        expenceZero.innerText = amount1
+        profitLossZero.innerText =   amount - amount1
+}
